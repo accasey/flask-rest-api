9c97081061dbce7ce945d1c8d1bd90a3bc0d5662
Andrew Casey <accasey@hotmail.com.au>
2020-08-02 15:35:00 +0100

    Finished

--- src/flask_rest_api/app.py
+++ src/flask_rest_api/app.py
@@ -7,9 +7,10 @@ import os
 # Application and database setup
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.join.path(
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
     basedir, "quotes.db"
 )
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 # Marshmallow library is used for effective object serialisation
@@ -66,13 +67,13 @@ def db_seed():
     db.session.add(quote4)
     db.session.add(quote5)
     db.session.commit()
-    print('Database seeded!')
+    print("Database seeded!")
 
 
 # database model
 class Quote(db.Model):
-    __tablename__ = 'quotes'
-    quote_id = Column(Integer, primary_key = True)
+    __tablename__ = "quotes"
+    quote_id = Column(Integer, primary_key=True)
     quote_desc = Column(String)
     quote_type = Column(String)
     author = Column(String)
@@ -81,9 +82,68 @@ class Quote(db.Model):
 # Quote model added to the Marshmallow library for JSON serialization
 class QuoteSchema(ma.Schema):
     class Meta:
-        fields = ('quote_id', 'quote_desc', 'quote_type', 'author')
+        fields = ("quote_id", "quote_desc", "quote_type", "author")
 
 
 quote_schema = QuoteSchema()
 quotes_schema = QuoteSchema(many=True)
+
+
+@app.route("/quotes", methods=["GET"])
+def quotes():
+    quotes_list = Quote.query.all()
+    result = quote_schema.dump(quotes_list)
+    return jsonify(result)
+
+
+@app.route("/quote_details/<int:quote_id>", method=["GET"])
+def quote_details(quote_id: int):
+    quote = Quote.query.filter_by(quote_id=quote_id).first()
+
+    if quote:
+        result = quote_schema.dump(quote)
+        return jsonify(result)
+    else:
+        return jsonify(message="That quote does not exist."), 404
+
+
+@app.route("/add_quote", method=["POST"])
+def add_quote():
+    quote_desc = request.form["quote_desc"]
+    test = Quote.query.filter_by(quote_desc=quote_desc).first()
+
+    if test:
+        return jsonify(message="There is already a quote by that description."), 409
+    else:
+        quote_type = request.form["quote_type"]
+        author = request.form["author"]
+        quote = Quote(quote_desc=quote_desc, quote_type=quote_type, author=author)
+        db.session.add(quote)
+        db.session.commit()
+        return jsonify(message="Quote added successfully!"), 201
+
+
+@app.route('/update_quote/<int:quote_id>', methods=['PUT'])
+def update_quote(quote_id: int):
+    quote = Quote.query.filter_by(quote_id=quote_id).first()
+    if quote:
+        quote.quote_desc = request.form['quote_desc']
+        quote.quote_type = request.form['quote_type']
+        quote.author = request.form['author']
+        db.session.commit()
+        return jsonify(message='Quote successfully updated!')
+    else:
+        return jsonify(message='That quote does not exist'), 404
+
+
+
+@app.route('/delete_quote/<int:quote_id>', methods=['DELETE'])
+def delete_quote(quote_id: int):
+    quote = Quote.query.filter_by(quote_id=quote_id).first()
+    if quote:
+        db.session.delete(quote)
+        db.session.commit()
+        return jsonify(message='Quote successfully deleted!')
+    else:
+        return jsonify(message='That quote does not exist!')
 
